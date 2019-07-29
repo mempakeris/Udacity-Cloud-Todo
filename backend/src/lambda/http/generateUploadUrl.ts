f3758cf6c47c39652d7f4cf34b4f0905db0e90bf
Matas <mempakeris@me.com>
2019-07-29 15:14:28 -0700

    generateUploadUrl added

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,19 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const s3 = AWS.S3();
+
+  // arguments to generate signed URL
+  const bucket = process.env.S3_BUCKET;
+  const urlExpiration = process.env.SIGNED_URL_EXPIRATION
   const todoId = event.pathParameters.todoId
 
-  // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-  return undefined
+
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucket,
+    Key: todoId,
+    Expires: urlExpiration
+  });
 }
