4b5979912e94e1d55ee9a1d7324c25109553a6b0
Matas <mempakeris@me.com>
2019-07-29 12:30:11 -0700

    getTodos added and serverless.yml iamRoleStatements updated

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,9 +1,25 @@
 import 'source-map-support/register';
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+  const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
+  // query variable
+  const tableName = process.env.TODOS_TABLE;
 
+  // get items from db
+  const items = await dynamoDB.scan({
+    TableName: tableName
+  }).promise();
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items
+    })
+  };
 }
