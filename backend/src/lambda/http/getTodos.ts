0b6df9d6a3d4c3c46a56d2b6ea3e1c073e52798c
Matas <mempakeris@me.com>
2019-07-31 17:05:37 -0700

    getTodos issues addressed

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -1,6 +1,13 @@
 import 'source-map-support/register';
-import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda';
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  APIGatewayProxyHandler
+} from 'aws-lambda';
 import * as AWS from 'aws-sdk';
+
+// dev imported
+import { getUserId } from '../utils';
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const dynamoDB = new AWS.DynamoDB.DocumentClient();
@@ -8,11 +15,16 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
   // query variable
   const tableName = process.env.TODOS_TABLE;
   const indexName = process.env.INDEX_NAME;
+  const userId = getUserId(event);
 
   // get items from db
-  const items = await dynamoDB.scan({
+  const result = await dynamoDB.scan({
     TableName: tableName,
-    IndexName: indexName
+    IndexName: indexName,
+    FilterExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': userId
+    }
   }).promise();
 
   return {
@@ -21,7 +33,7 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
       'Access-Control-Allow-Origin': '*'
     },
     body: JSON.stringify({
-      items
+      items: result.Items
     })
   };
 }
